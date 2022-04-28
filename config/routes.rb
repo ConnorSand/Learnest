8ad58e26b1457408097166f9ad119d97a81ea8ca
Connor Sanderson <connor.sanderson@outlook.com>
2022-04-28 20:26:35 +1000

    added initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/about", to: "pages#about"
+
+  resources :questions, only: %i[index new create show edit update] do
+    resources :answers, only: %i[new create edit update]
+  end
+  resources :universities, only: %i[index new create show edit update]
 end

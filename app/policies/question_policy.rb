c9bcaddc135d8705fd3c7a8d47e7a6a6ca6a0b8a
Connor Sanderson <connor.sanderson@outlook.com>
2022-04-29 22:58:39 +1000

    populated questions controller and question policy - not fully working

--- app/policies/question_policy.rb
+++ app/policies/question_policy.rb
@@ -1,8 +1,38 @@
 class QuestionPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    user.present?
+  end
+
+  def create?
+    user.present?
+  end
+
+  def edit?
+    user.present? && (owner? || admin?)
+  end
+
+  def update?
+    user.present? && (owner? || admin?)
+  end
+
+  private
+
+  def owner?
+    record.user == user
+  end
+
+  def admin?
+    user.admin
   end
 end

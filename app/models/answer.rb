cc122ab4de57f789ae5afc785e12997a12dcc590
ashmeet khurana <ashmeetkhurana@gmail.com>
2022-04-29 15:39:33 +1000

    models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,8 @@
 class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
+
+  validates :user, presence: true
+  validates :question, presence: true
+  validates :content, length: { maximum: 100 }
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
+  has_many :answers
+
+  validates :user, presence: true
+  validates :content, length: { maximum: 50 }
 end

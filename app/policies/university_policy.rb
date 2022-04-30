1cf62b5451539ee08e45b3f95c7809df45c58699
ashmeet khurana <ashmeetkhurana@gmail.com>
2022-04-30 15:38:21 +1000

    updateduniversitycontroller

--- app/policies/university_policy.rb
+++ app/policies/university_policy.rb
@@ -1,8 +1,23 @@
 class UniversityPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
   end
-end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.present?
+  end
+
+  def edit?  
+    user.present?
+  end
+
+  def update?
+    user.present?
+  end

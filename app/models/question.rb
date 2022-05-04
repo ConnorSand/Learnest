e78689c976dc14db1b04b2c1bfdfd767e6a3736c
Brian Huynh <vuduchv@gmail.com>
2022-05-04 10:38:45 +1000

    Search function

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,9 @@ class Question < ApplicationRecord
 
   validates :user, presence: true
   validates :content, length: { maximum: 100 }
+
+  include PgSearch::Model
+  pg_search_scope :global_search, against: [:content], associated_against: { answers: [:content] }, using: {
+    tsearch: { prefix: true }
+  }
 end
